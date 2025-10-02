60bf18381056caa0448710823dad3dd86cab3c45
velocity968 <velo45145@gmail.com>
2025-10-02 16:29:19 +0200

    Added admin functionalities 2/2
    Added missing part of admin functionalities from last commit.

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -5,10 +5,17 @@ namespace Grocery.Core.Models
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public Role Role { get; set; } = Role.None;
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
             EmailAddress=emailAddress;
             Password=password;
         }
     }
+}
+
+public enum Role
+{
+    None,
+    Admin
 }
